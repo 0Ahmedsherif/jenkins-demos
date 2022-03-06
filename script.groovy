7cbbad3cbb39e1e5bfb8b8ec3773009424819a63
Ahmedib0299@gmail.com <Ahmedib0299@gmail.com>
2022-03-06 12:21:16 +0000

    new version

--- script.groovy
+++ script.groovy
@@ -7,8 +7,8 @@ def buildimage() {
   echo "Building the docker image..."
   withCredentials([usernamePassword(credentialsId: 'docker_hub', passwordVariable: 'PWD', usernameVariable: 'USER')]) {
     sh "echo $PwD | docker login -u $USER --password-stdin"
-    sh 'docker build -t doc299/java-maven-app:1.1 .'
-    sh 'docker push doc299/java-maven-app:1.1'
+    sh 'docker build -t doc299/java-maven-app:2.2 .'
+    sh 'docker push doc299/java-maven-app:2.2'
   }
 }
 
